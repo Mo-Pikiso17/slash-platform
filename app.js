c4c027c77a3200a93c193dbe4a6bf4691fa57cdd
Mthobisi-za <mthobisimtho333@gmail.com>
2021-09-01 21:37:07 +0200

    push

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const exhbs = require("express-handlebars");
+const body = require("body-parser");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(body.urlencoded({extended: false}));
+app.use(body.json());
 app.use(express.static('public'));
 app.engine("handlebars", exhbs({defaultLayout: "main", layoutsDir: "views/layouts"}));
 app.set("view engine", "handlebars");
